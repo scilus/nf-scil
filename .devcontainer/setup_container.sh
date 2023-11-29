dedcb56aef11c0a3af73c83eccf516d4a954b4f7
Alex Valcourt Caron <alex.valcourt.caron@usherbrooke.ca>
2023-11-29 10:09:54 -0500

    bind venv in container to volume (speeds up container uptime)

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir /nf-scil-poetry-venv
-ln -s /nf-scil-poetry-venv .venv
-#poetry install --no-root
-#echo "export PROFILE=docker" >> ~/.bashrc
+poetry install --no-root
+echo "export PROFILE=docker" >> ~/.bashrc
 
-#
