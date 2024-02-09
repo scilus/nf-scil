95f02451f6608951abe2c97979bca34dfc47696c
AlexVCaron <alex.valcourt.caron@usherbrooke.ca>
2024-02-09 16:43:25 +0000

    editorconfig fix

--- .devcontainer/setup_container.sh
+++ .devcontainer/setup_container.sh
@@ -8,6 +8,8 @@ echo "export PROFILE=docker" >> ~/.bashrc
 curl -fsSL https://deb.nodesource.com/setup_${NODE_MAJOR}.x | bash - &&\
 apt-get install -y nodejs
 
-npm install --save-dev --save-exact prettier
+npm install -g --save-dev --save-exact prettier
+npm install -g editorconfig
+npm install -g --save-dev editorconfig-checker
 
 echo "function prettier() { npm exec prettier $@; }" >> ~/.bashrc
