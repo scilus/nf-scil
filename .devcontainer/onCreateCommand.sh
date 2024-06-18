4691596f1d15a71d86502c36ac93fd2c79be136a
AlexVCaron <alex.valcourt.caron@usherbrooke.ca>
2024-06-18 19:49:03 +0000

    fix nf-scil as main remote for nf-core modules and subworkflows commands

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-poetry install --no-root
-echo "export PROFILE=docker" >> ~/.bashrc
-
 npm install -g --save-dev --save-exact prettier
 npm install -g editorconfig
 npm install -g --save-dev editorconfig-checker
+
+mkdir -p $XDG_CONFIG_HOME/nf-scil
+touch $XDG_CONFIG_HOME/nf-scil/.env
+echo "source $XDG_CONFIG_HOME/nf-scil/.env" >> ~/.bashrc
