37856b238c9b0d5c9e939d44df5c1dc0df0ad6e5
AlexVCaron <alex.valcourt.caron@usherbrooke.ca>
2024-06-19 19:48:07 +0000

    move nf-test installation to container

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -7,3 +7,8 @@ npm install -g --save-dev editorconfig-checker
 mkdir -p $XDG_CONFIG_HOME/nf-scil
 touch $XDG_CONFIG_HOME/nf-scil/.env
 echo "source $XDG_CONFIG_HOME/nf-scil/.env" >> ~/.bashrc
+
+mkdir -p /nf-test/bin
+cd /nf-test/bin/
+curl -fsSL https://code.askimed.com/install/nf-test | bash -s ${NFTEST_VERSION}
+echo "PATH=$PATH:/nf-test/bin" >> ~/.bashrc
